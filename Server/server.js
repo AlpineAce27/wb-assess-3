901fb6b5d537decdf8761d17c8d8707ce0924a6e
Kimball Goss <Kimballgoss@gmail.com>
2024-07-02 09:55:59 -0600

    added front end

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import axios from 'axios';
+//import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
@@ -32,6 +32,7 @@ let globalID = 5
 
 app.get('/', (req, res) => {
     console.log('/ endpoint hit')
+    displayAllTasks(toDoList)
     res.send(toDoList)
     res.render('index.html')
 })
@@ -42,6 +43,8 @@ app.post('/newItem', (req, res) => {
         id: globalID,
         task : req.body.task
     }
+    console.log(newItem)
+    toDoList.push(newItem)
     //add that list item to the toDoList object
     res.send(toDoList)
     //res.render('index.html')
@@ -49,18 +52,19 @@ app.post('/newItem', (req, res) => {
 
 app.delete('/removeItem', (req, res) => {
     console.log('/removeItem endpoint hit')
-    //grab data from the input form (checkbox on each item)
+    
+    console.log(req.body.id)
     //find the item with a corresponding id and remove it from the toDoList object
     res.send(toDoList)
     //res.render('index.html')
 })
 
-app.put('/editItem', (req, res) => {
-    console.log('/editItem endpoint hit')
-    //grab the data from input for (item ID and new text)
-    //find the item with a a corresponding id and change the text to reflect the changes
-    res.send(toDoList)
-    //res.render('index.html')
-})
+// app.put('/editItem', (req, res) => {
+//     console.log('/editItem endpoint hit')
+//     //grab the data from input for (item ID and new text)
+//     //find the item with a a corresponding id and change the text to reflect the changes
+//     res.send(toDoList)
+//     //res.render('index.html')
+// })
 
 app.listen(port, () => console.log(`Flying high at http://localhost:${port}`))
---
+++ src/index.js
@@ -0,0 +1,50 @@
+//import axios from 'axios';
+
+console.log('javascript connected')
+
+const listDisplay = document.querySelector('#to-do-list')
+
+const displayAllTasks = (array) => {
+    //clear out whats on the page
+    drinkDisplay.innerHTML = ``
+    
+    array.forEach((el) => {
+        const itemLine = document.createElement('section')
+        itemLine.innerHTML = `
+        <label for="${el.id}">${el.task}</label>
+        <button name="${el.id} onclick="deleteTask(${el.id})">X</button>
+        <br/>
+    `
+    itemLine.appendChild(listDisplay)
+    })
+}
+
+const allTasks = () => {
+    axios.get('http://localhost:8000/').then((response) => {
+        console.log(response.data)
+        displayAllTasks(response.data)
+    })
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const taskName = document.querySelector('#taskName').value
+    const bodyObj = {name: taskName}
+
+    axios.post('http://localhost:8000/newItem', bodyObj).then((response) => {
+        console.log(response.data)
+        displayAllDrinks(response.data)
+    })
+}
+
+//const createNewListItem = document.querySelector('#add-task').addEventListener('submit', handleSubmit)
+
+const deleteTask = ((id) => {
+    axios.delete(`http://localhost:8000/task/${id}`).then((response) => {
+        console.log(response.data)
+        displayAllTasks(response.data)
+    })
+})
+
+allTasks()
